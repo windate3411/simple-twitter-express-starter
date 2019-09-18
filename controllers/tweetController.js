efd07bfa90e6e00a217bb03f725523baf315fa86
Danny <windate3411@gmail.com>
2019-09-18 21:22:01 +0800

    modify sent messages

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -5,29 +5,60 @@ const Tweet = db.Tweet
 const tweetController = {
   //瀏覽所有推播
   getTweets: async (req, res) => {
-    const tweets = await Tweet.findAll()
-    return res.json(tweets)
+    try {
+      const tweets = await Tweet.findAll()
+      return res.json({ status: 'success', tweets, message: 'Successfully getting the tweets' })
+    } catch (error) {
+      return res.json({ status: 'error', message: error })
+    }
   },
   //新增一則推播
   postTweet: async (req, res) => {
-    //若推播內容為空或是大於140字元則報錯
-    if (!req.body.description || Number(req.body.description.length) > 140) {
-      return res.json({ status: 'error', message: "invalid tweet content" })
-    } else {
-      await Tweet.create({
-        description: req.body.description,
-        UserId: req.user.id
-      });
-      return res.json({ status: 'success', message: "new tweet has been successfully created" })
+    try {
+      //若推播內容為空或是大於140字元則報錯
+      if (!req.body.description || Number(req.body.description.length) > 140) {
+        return res.json({ status: 'error', message: "invalid tweet content" })
+      } else {
+        await Tweet.create({
+          description: req.body.description,
+          UserId: req.user.id
+        });
+        return res.json({ status: 'success', message: "new tweet has been successfully created" })
+      }
+    } catch (error) {
+      return res.json({ status: 'error', message: error })
     }
-
   },
   //編輯一則推播
   putTweet: async (req, res) => {
-    //若推播內容為空或是大於140字元則報錯
-    if (!req.body.description || Number(req.body.description.length) > 140) {
-      return res.json({ status: 'error', message: "invalid tweet content" })
-    } else {
+    try {
+      if (!req.body.description || Number(req.body.description.length) > 140) {
+        return res.json({ status: 'error', message: "invalid tweet content" })
+      } else {
+
+        const tweet = await Tweet.findByPk(req.params.id)
+
+        //若找不到指定的推播則報錯
+        if (tweet === null) {
+          return res.json({ status: 'error', message: "tweet was not found" })
+        }
+
+        //若非本人則報錯
+        if (req.user.id != tweet.UserId) {
+          return res.json({ status: 'error', message: "you are not authorized to do this action" })
+        }
+
+        tweet.update(req.body)
+        return res.json({ status: 'success', message: "category was successfully  updated" })
+      }
+
+    } catch (error) {
+      return res.json({ status: 'error', message: error })
+    }
+  },
+  //刪除一則推播
+  deleteTweet: async (req, res) => {
+    try {
       const tweet = await Tweet.findByPk(req.params.id)
 
       //若找不到指定的推播則報錯
@@ -40,26 +71,11 @@ const tweetController = {
         return res.json({ status: 'error', message: "you are not authorized to do this action" })
       }
 
-      tweet.update(req.body)
-      return res.json({ status: 'success', message: "category was successfully  updated" })
+      tweet.destroy()
+      return res.json({ status: 'success', message: "tweet was successfully destoryed" })
+    } catch (error) {
+      return res.json({ status: 'error', message: error })
     }
-  },
-  //刪除一則推播
-  deleteTweet: async (req, res) => {
-    const tweet = await Tweet.findByPk(req.params.id)
-
-    //若找不到指定的推播則報錯
-    if (tweet === null) {
-      return res.json({ status: 'error', message: "tweet was not found" })
-    }
-    //若非本人則報錯
-    if (req.user.id != tweet.UserId) {
-      return res.json({ status: 'error', message: "you are not authorized to do this action" })
-    }
-
-    tweet.destroy()
-    return res.json({ status: 'success', message: "tweet was successfully destoryed" })
-
   }
 }
 
