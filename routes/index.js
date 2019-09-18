a366630be5450f239e11971a4a30cfde62f4d381
Danny <windate3411@gmail.com>
2019-09-18 22:16:38 +0800

    modify put/delete endpoint and route/index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ module.exports = (app) => {
   app.get('/', ensureAuthenticated, getUser, (req, res) => res.send('Hello World!'))
   app.use('/users', userRoute)
   app.use('/tweets', ensureAuthenticated, getUser, tweetRoute)
-}
   app.use('/admin', ensureAuthenticated, getUser, isAuthAdmin, adminRoute)
   app.use('/followships', ensureAuthenticated, getUser, followshipRoute)
 }
