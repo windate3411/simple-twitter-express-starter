92ee98d8c0fcaed2e7752007cc651a4269af0f19
Mike <mike.huang.mikank@gmail.com>
2019-09-18 21:24:19 +0800

    Add follow feature and update helper config

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const userRoute = require('./userRoute')
+const followshipRoute = require('./followshipRoute')
 const { ensureAuthenticated, isAuthAdmin, getUser } = require('../config/auth')
 
 module.exports = (app) => {
   app.get('/', ensureAuthenticated, getUser, (req, res) => res.send('Hello World!'))
   app.use('/users', userRoute)
+  app.use('/followships', ensureAuthenticated, getUser, followshipRoute)
 }
