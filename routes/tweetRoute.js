c2172e1f51b19e3bce4a1336798514e9b4c196ac
Danny <windate3411@gmail.com>
2019-09-18 18:11:41 +0800

    merged origin feature/tweet_crud

--- routes/tweetRoute.js
+++ routes/tweetRoute.js
@@ -5,11 +5,13 @@ const tweetController = require('../controllers/tweetController')
 //瀏覽所有推播
 router.get('/', tweetController.getTweets)
 
+
 // 新增一則推播
 router.post('/', tweetController.postTweet)
 
 // 編輯一則推播
 router.put('/:id', tweetController.putTweet)
+
 
 //刪除一則推播
 router.delete('/:id', tweetController.deleteTweet)
