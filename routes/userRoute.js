daf92b2517b6d992aac98b9d01f8aadb9b468b0e
Danny <windate3411@gmail.com>
2019-09-19 12:10:33 +0800

    finish getUser,getUserPage,editUser actions

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
-
+const { ensureAuthenticated, isAuthAdmin, getUser } = require('../config/auth')
 const userController = require('../controllers/userController')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 
 router.post('/signup', userController.signUp)
 router.post('/login', userController.logIn)
+router.get('/:id/tweets', ensureAuthenticated, userController.getUser)
+router.get('/:id/edit', ensureAuthenticated, userController.getUserPage)
+router.post('/:id/edit', ensureAuthenticated, upload.single('avatar'), userController.editUserPage)
 
 module.exports = router
