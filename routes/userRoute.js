b582d31bb46f5af782b7a30f5b28c0f9eb57e2b0
Mike <mike.huang.mikank@gmail.com>
2019-09-20 11:47:53 +0800

    Add route protections and helper to user routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { ensureAuthenticated, isAuthAdmin, getUser } = require('../config/auth')
+const { ensureAuthenticated, getUser } = require('../config/auth')
 const userController = require('../controllers/userController')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
@@ -8,10 +8,10 @@ const upload = multer({ dest: 'temp/' })
 
 router.post('/signup', userController.signUp)
 router.post('/login', userController.logIn)
-router.get('/:id/tweets', ensureAuthenticated, userController.getUser)
-router.get('/:id/edit', ensureAuthenticated, userController.getUserPage)
-router.post('/:id/edit', ensureAuthenticated, upload.single('avatar'), userController.editUserPage)
-router.get('/:userId/likes', userController.getLikes)
-router.get('/current_user', ensureAuthenticated, userController.getCurrentUser)
+router.get('/:id/tweets', ensureAuthenticated, getUser, userController.getUser)
+router.get('/:id/edit', ensureAuthenticated, getUser, userController.getUserPage)
+router.post('/:id/edit', ensureAuthenticated, getUser, upload.single('avatar'), userController.editUserPage)
+router.get('/:userId/likes', ensureAuthenticated, getUser, userController.getLikes)
+router.get('/current_user', ensureAuthenticated, getUser, userController.getCurrentUser)
 
 module.exports = router
