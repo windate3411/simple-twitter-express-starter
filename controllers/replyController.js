996d516c2732f63cf2de4d739632d91d45518d09
Mike <mike.huang.mikank@gmail.com>
2019-09-21 15:01:48 +0800

    Check if comment exists before handle reply

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -1,12 +1,14 @@
 const db = require('../models')
 const Reply = db.Reply
 
-const replyController ={
+const replyController = {
   // reply 的 user 可以修改 reply
   putReply: async (req, res) => {
     try {
       //找到 reply
       let comment = await Reply.findByPk(req.params.reply_id)
+      // check if reply exists
+      if (!comment) return res.status(404).json({ status: 'error', message: 'Cannot find the comment' })
       const tweet_id = comment.TweetId
       // 確認是否是 reply 的發布者
       if (req.user.id !== comment.UserId) {
@@ -16,9 +18,10 @@ const replyController ={
       if (!req.body.comment) {
         return res.status(400).json({ status: 'error', message: 'comment can not be empty.' })
       }
-      await comment.update({comment: req.body.comment})
+      await comment.update({ comment: req.body.comment })
       return res.status(201).json({ status: 'success', tweet_id, message: 'reply has been successfully edited.' })
     } catch (error) {
+      console.log(error)
       return res.status(500).json({ status: 'error', message: error })
     }
   },
@@ -27,12 +30,14 @@ const replyController ={
     try {
       // 找 reply
       let comment = await Reply.findByPk(req.params.reply_id)
+      // check if the comment exists
+      if (!comment) return res.status(404).json({ status: 'error', message: 'Cannot find the comment' })
       // 確認是否有權限刪除
       if ((req.user.id !== comment.UserId) && (req.user.role !== 'Admin')) {
         return res.status(400).json({ status: 'error', message: 'you are not authorized to do this action.' })
       }
       await comment.destroy()
-      return res.status(202).json({status: 'success', message: 'reply was successfully destroyed'})
+      return res.status(202).json({ status: 'success', message: 'reply was successfully destroyed' })
     } catch (error) {
       return res.status(500).json({ status: 'error', message: error })
     }
